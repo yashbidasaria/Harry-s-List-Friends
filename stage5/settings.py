e23ab837493510135e18b52a5e3f4589066b7dc8
Yash Bidasaria <bidasaria@wisc.edu>
2017-12-04 17:56:19 -0600

    Basic HTML

--- stage5/settings.py
+++ stage5/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'stage5.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,7 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 

19a4d4151ecc7ed7e43de1399f72e687f5023d21
Evan <evanjordandweck@gmail.com>
2017-12-04 21:29:17 -0600

    added the user registration/login/logout system.  UI needs work still. WIP

--- harryslist/views.py
+++ harryslist/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from .forms import UserRegistrationForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -27,7 +33,7 @@ def signup(request):
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=rawpassword)
+            user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('index')
     else:
--- stage5/urls.py
+++ stage5/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('harryslist.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
 ]

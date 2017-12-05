0470d5a702e41463c7f71b5f082f3a517d276d9a
Evan <evanjordandweck@gmail.com>
2017-12-04 19:07:07 -0600

    added html files for other pages as well as view functions and urls for each page.  Also connected bootstrap for better UI experience (we are all about the experience at harrys list)

--- harryslist/urls.py
+++ harryslist/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^top_artists/$', views.top_artists, name='top_artists'),
+    url(r'^top_albums/$', views.top_albums, name='top_albums'),
+    url(r'^top_songs/$', views.top_songs, name='top_songs'),
+    url(r'^about/$', views.about, name='about'),
 ]
--- harryslist/views.py
+++ harryslist/views.py
@@ -8,6 +8,14 @@ from django.views.generic.base import TemplateView
 def index(request):
     return render(request, 'index.html')
 
-class HomeView(TemplateView):
+def top_artists(request):
+    return render(request, 'top_artists.html')
 
-    template_name = 'index.html'
+def top_albums(request):
+    return render(request, 'top_albums.html')
+
+def top_songs(request):
+    return render(request, 'top_songs.html')
+
+def about(request):
+    return render(request, 'about.html')

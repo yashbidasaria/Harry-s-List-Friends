548f713e35df309e89d53f71b8935d31bf6100f9
Evan <evanjordandweck@gmail.com>
2017-12-04 20:02:28 -0600

    created a base template to extend other html files for

--- harryslist/views.py
+++ harryslist/views.py
@@ -19,3 +19,17 @@ def top_songs(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=rawpassword)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})

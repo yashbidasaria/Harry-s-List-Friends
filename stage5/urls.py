42bf76160b845a53c70d50558e50ca90d0a35c4a
Evan <evanjordandweck@gmail.com>
2017-12-05 18:53:07 -0600

    added static files for css

--- stage5/urls.py
+++ stage5/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('harryslist.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

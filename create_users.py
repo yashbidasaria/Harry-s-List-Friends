0f0c4bfe00ad76e51428c50c832cc3b7a6412c46
Evan <evanjordandweck@gmail.com>
2017-12-14 23:04:01 -0600

    adding script to populate auth_user table with tuples & appropriate CSV to populate

---
+++ create_users.py
@@ -0,0 +1,16 @@
+import csv
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+#get the data from csv
+with open('mock_users.csv') as csvDataFile:
+    csvReader = csv.reader(csvDataFile)
+    for row in csvReader:
+        user_name = str(row[-(len(row)-2):-2])
+        pass_word = 'adeladel'
+        if not UserModel.objects.filter(username=user_name).exists():
+            user=UserModel.objects.create_user(user_name, password = pass_word)
+            user.save()
+
+
